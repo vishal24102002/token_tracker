7c35ac32f78bb1aadbe945ce93956ac45a235800
Vishal sharma <103731436+vishal24102002@users.noreply.github.com>
2025-06-07 14:23:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def check_token_prices():
             conn = get_db_connection()
             tokens_detail_in = conn.execute(f"SELECT * FROM history where mint_address = ?",(token['mint_address'],)).fetchall()
             tokens_detail_out = conn.execute(f"SELECT * FROM history where mint_address = ?",(token['output_mint'],)).fetchall()
-            con.close()
+            conn.close()
             price = fetch_price(token['mint_address'], token['output_mint'])
             if price is None:
                 continue
